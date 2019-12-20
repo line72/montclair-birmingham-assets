341656338964449149150b784ece5ae7bc67b13a
Marcus Dillavou <line72@line72.net>
2019-12-20 16:08:59 -0600

    Initial commit

---
+++ assets/Configuration.js
@@ -0,0 +1,31 @@
+/* -*- Mode: rjsx -*- */
+
+/*******************************************
+ * Copyright (2018)
+ *  Marcus Dillavou <line72@line72.net>
+ *  http://line72.net
+ *
+ * Montclair:
+ *  https://github.com/line72/montclair
+ *  https://montclair.line72.net
+ *
+ * Licensed Under the GPLv3
+ *******************************************/
+
+import AvailtecParser from './AvailtecParser';
+
+class Configuration {
+    constructor() {
+        // Birmingham, AL
+        this.center = [33.5084801, -86.8006611];
+
+        this.agencies = [
+            {
+                name: 'Routes',
+                parser: new AvailtecParser('https://realtimebjcta.availtec.com/InfoPoint')
+            }
+        ];
+    }
+}
+
+export default Configuration;
