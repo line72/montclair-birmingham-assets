2ab7796f6bc4f5302ad752c1bba72a260fdc2586
Marcus Dillavou <line72@line72.net>
2020-01-01 17:12:48 -0700

    Changed the name to Go Birmingham Changed the domain to birmingham.gotransitapp.com Updated the logo Added in CI through github actions

--- config.py
+++ config.py
@@ -6,28 +6,28 @@ CONFIG = transmogrifier.Config(
     build_dir = './build',
     repo = 'birmingham',
     package_name = 'montclair-birmingham',
-    name = 'Birmingham Transit',
-    description = 'Real Time Bus Tracking for Birmingham, AL',
-    url = 'https://montclair.line72.net',
+    name = 'Go Birmingham',
+    description = 'Real Time Tracking of the Buses for Birmingham, AL',
+    url = 'https://birmingham.gotransitapp.com',
     logo_svg = 'assets/logo.svg',
     montclair_config = transmogrifier.MontclairConfig(
-        version = '1.2.1',
-        revision = 3,
-        title = 'Birmingham Transit',
+        version = '1.2.2',
+        revision = 1,
+        title = 'Go Birmingham',
         first_run_text = "Welcome to Birmingham, AL's Real Time Bus Tracker.<br /><br />Please select one or more routes to begin!",
         configuration_js_file = 'assets/Configuration.js'
     ),
     ios_config = transmogrifier.MontclairiOSConfig(
-        version = '1.0.0-3',
+        version = '2.0.2',
         revision = 1,
-        app_id = 'net.line72.montclair',
-        app_store_id = 'id1418953482',
-        app_store_url = 'https://itunes.apple.com/us/app/montclair/id1418953482'
+        app_id = 'com.gotransitapp.birmingham',
+        app_store_id = '',
+        app_store_url = 'REPLACE_ME'
     ),
     android_config = transmogrifier.MontclairAndroidConfig(
-        version = '1.0.0-2',
+        version = '1.0.2',
         revision = 1,
-        app_id = 'net.line72.montclair',
-        play_store_url = 'https://play.google.com/store/apps/details?id=net.line72.montclair'
+        app_id = 'com.gotransitapp.birmingham',
+        play_store_url = 'https://play.google.com/store/apps/details?id=com.gotransitapp.birmingham'
     )
 )
