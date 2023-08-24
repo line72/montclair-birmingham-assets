2cdd50afa35efc3ecd296056929398eed9858943
Marcus Dillavou <line72@line72.net>
2023-08-23 19:04:59 -0500

    Build Android 1.2.0-2 with new Transmogrifier

--- config.py
+++ config.py
@@ -26,7 +26,7 @@ CONFIG = transmogrifier.Config(
     ),
     android_config = transmogrifier.MontclairAndroidConfig(
         version = '1.2.0',
-        revision = 1,
+        revision = 2,
         app_id = 'com.gotransitapp.birmingham',
         play_store_url = 'https://play.google.com/store/apps/details?id=com.gotransitapp.birmingham'
     )
